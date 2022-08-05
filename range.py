4d98bca1724b725152019c856e9984509c4edc54
saikiran0018 <109685878+saikiran0018@users.noreply.github.com>
2022-08-05 19:07:23 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,47 @@
+# a=range(5)
+# print(a)                     #  range(0,5)
+ 
+# for i in range(5):
+#     print(i)                      # vertical 0 to 4
+
+# for i in range(1,11):
+#     print(i)                    # vertical 1to 10
+
+# for i in range(10,11):
+#     print(i)                       # 10
+
+
+# for i in range(20,11):
+#     print(i)                    # IndentationError: expected an indented block after 'for' statement on line 1    
+
+# for i in range(1,21):
+#     print(i,end="")                 # 1234567891011121314151617181920
+
+# for i in range(1,21):
+#     print(i,end=" ")                   # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+
+# for i in range(1,20,2):
+#     print(i,end=" ")                        # 1 3 5 7 9 11 13 15 17 19 
+
+# for i in range(2,21,2):
+#     print(i,end=" ")                         # 2 4 6 8 10 12 14 16 18 20 
+
+# for i in range(10,0,-1):
+#     print(i,end=" ")                           # 10 9 8 7 6 5 4 3 2 1
+
+# for i in range(20,0,-2):
+#     print(i,end=" ")                            #  20 18 16 14 12 10 8 6 4 2
+
+# for i in range(19,0,-2):
+#     print(i,end=" ")                            # 19 17 15 13 11 9 7 5 3 1 
+
+# a=1,2,3,4
+# print(*a)                                          # 1,2,3,4
+
+# a=10,20,30
+# for josh in a:
+#     print(josh,end=" ")                           # 10,20,30
+
+# n=int(input("enter any integer n:"))
+# for i in range(1,n+1):
+#     print(i,end="\n")
